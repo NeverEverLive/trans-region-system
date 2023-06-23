830c73849bf169d0e047a1d44d0221504f1efba4
NeverEverLive <nevereverlives@gmail.com>
2023-06-23 09:11:15 +0300

    change env for docker and add try on database connection

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,10 @@
+import logging
 from src.models.base import get_session, set_session
 from src.models.user import UserModel
 from src.models.project import Project
 
-set_session()
+try:
+    set_session()
+except Exception as e:
+    logging.error(e.args)
+    logging.error("You have some problems with database")
